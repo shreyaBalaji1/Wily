c964866d295aa3d7c55d2771d6a0885284bc46e6
Shreya <123@gmail.com>
2021-02-23 18:03:23 -0600

    Class 74

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,12 +1,60 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import db from '../config';
+import {ScrollView} from "react-native-gesture-handler";
 
 export default class SearchScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            allTransactions: [],
+            lastVisibleTransaction: null
+        }
+    }
+
+    componentDidMount = async() => {
+        const query = await db.collection("transaction").get();
+        query.docs.map((doc) => {
+            this.setState({
+                allTransactions: [...this.state.allTransactions, doc.data()]
+            });
+        });
+    }
+
+    fetchMoreTransactions = async() => {
+        const query = await db.collection("transaction").startAfter(this.state.lastVisibleTransaction).limit(10).get();
+        query.docs.map((doc) => {
+            this.setState({
+                allTransactions: [...this.state.allTransactions, doc.data()],
+                lastVisibleTransaction: doc
+            });
+        });
+    }
+
     render() {
         return(
-            <View style={{flex:1, justifyContent: "center", alignItems: "center"}}>
-                <Text style = {{fontSize: 20}}>Search</Text>
+        <View>
+            
+            <View>
+
             </View>
+            <FlatList>
+                data = {this.state.allTransactions}
+                renderItem = {({item}) => {
+                    <View style = {{borderBottomWidth: 2}} key = {index}>
+                        <Text>{"Book ID: " + item.bookId}</Text>
+                        <Text>{"Student ID: " + item.studentId}</Text>
+                        <Text>{"Date: " + item.date.toDate()}</Text>
+                        <Text>{"Transaction type: " + item.transactionType}</Text>
+                    </View>
+                }}
+                keyExtractor = {(item, index) => {
+                    index.toString();
+                }}
+                onEndReached = {this.fetchMoreTransactions}
+                onEndThreshold = {0.7}
+            </FlatList>
+        </View>
         );
     }
 }
